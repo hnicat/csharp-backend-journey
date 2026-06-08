2cb302c13d5175164734ce1f09ab095b3d6d78b8
hnicat <hnicat70@gmail.com>
2026-06-08 22:25:51 +0400

    First Commit

---
+++ lesson1/testt.cs
@@ -0,0 +1,18 @@
+using System;
+namespace test{
+public class Program{
+public static void Main(){
+Console.WriteLine("Zehmet Olmasa Adinizi Qeyid Edin:");
+string name=Console.ReadLine();
+Console.WriteLine(name+" Salam");
+Console.ReadLine();
+
+}
+
+
+
+}
+
+
+
+}
