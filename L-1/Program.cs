80cb3a033f0de6cec73f40f8d456f4f4e0b9584e
hnicat <hnicat70@gmail.com>
2026-06-09 03:12:56 +0400

    Today I reviewed the basics of C#, including data types, variables, the difference between var and object, and fundamental Git concepts.

---
+++ L-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geridonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string senteces = "Salam Gencler";
+            //byte age = 15;// 255
+
+
+
+            //int x = 270;
+            //long y = 66666666666666666;
+            //Console.WriteLine(age+20);
+            //Console.WriteLine(senteces);
+
+            //Console.WriteLine("Zehmet Olmasa Adizi Daxil Edin:");
+            //string fullname = Console.ReadLine();
+            //Console.WriteLine("Zehmet Olmasa Yasinzi Daxil Edin:");
+            //byte age = Convert.ToByte(Console.ReadLine());
+            //Console.WriteLine(fullname + " "+ age);
+
+            //Console.WriteLine("1 ci Ededi Daxil Edin");
+            //int x1=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("2 ci Ededi Daxil Edin");
+            //int x2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Netice:");
+            //int resuat = x1 + x2;
+            //Console.WriteLine(resuat);
+        }
+    }
+}
